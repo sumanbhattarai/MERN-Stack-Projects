8925620300047cb114c9e05fca8f391f23ef6430
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-02-08 19:18:41 +0545

    intial commit

---
+++ first-mern-app/server.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express();
+const port = 5000 ;
+
+app.get('api/student' , (req , res)=>{
+    const student = [
+        {
+            id : 1 ,
+            name : 'Suman Bhattarai' ,
+            age : 21
+        } ,
+        {
+            id : 2 ,
+            name : 'Anil Tiwari' ,
+            age : 20
+        } ,
+        {
+            id : 3 ,
+            name : 'Mikal Poudel' ,
+            age : 19
+        } ,
+        {
+            id : 4 ,
+            name : 'Sujan Bhattarai' ,
+            age : 22
+        }
+    ];
+    res.send(student) ;
+})
+
+app.listen(port , ()=>{
+    console.log('Server running at : ' + port ) ;
+})
